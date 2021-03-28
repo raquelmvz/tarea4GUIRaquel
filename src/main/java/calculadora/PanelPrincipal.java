0d83734a9e0b359d7fbc95bccf45db4a5b4b4ab8
raquelmvz <raquel.mvz98@gmail.com>
2021-03-28 17:10:42 +0200

    Crea suma de varios elementos (primera idea)

--- src/main/java/calculadora/PanelPrincipal.java
+++ src/main/java/calculadora/PanelPrincipal.java
@@ -9,6 +9,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -23,6 +24,7 @@ public class PanelPrincipal extends JPanel {
     private PanelBotones botonera;
     private JTextArea areaTexto;
     private int tipoOperacion;
+    private String operando;
 
     // Constructor
     public PanelPrincipal() {
@@ -45,7 +47,11 @@ public class PanelPrincipal extends JPanel {
         this.add(areaTexto, BorderLayout.NORTH);
         this.add(botonera, BorderLayout.SOUTH);
 
+        ArrayList<String> operandos = new ArrayList<>();
+        operando = "";
+
         for (JButton boton : this.botonera.getgrupoBotones()) {
+
             boton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent ae) {
@@ -53,13 +59,47 @@ public class PanelPrincipal extends JPanel {
                     //Objeto que se acciona
                     Object obj = ae.getSource();
 
+                    //el objeto es un boton
                     if (obj instanceof JButton) {
 
-                        if (((JButton) obj).getText() != "+" && ((JButton) obj).getText() != "-"
-                                && ((JButton) obj).getText() != "=") {
-                            areaTexto.setText(areaTexto.getText() + (((JButton) obj).getText()));
-                        }
+//                        if (((JButton) obj).getText() != "+") {
+//                            operando += ((JButton) obj).getText();
+//                            areaTexto.setText(areaTexto.getText() + ((JButton) obj).getText());
+//                        }
+//
+//                        System.out.println("operando:  " + operando);
+                        String contenidoBoton = ((JButton) obj).getText();
+                        //System.out.println("contenido -- " + contenidoBoton);
 
+                        if (contenidoBoton == "+") {
+                            //System.out.println("operando --" + operando);
+                            operandos.add(operando);
+                            //System.out.println("lsita -- " + operandos.toString());
+                            operando = "";
+                            areaTexto.setText(areaTexto.getText() + ((JButton) obj).getText());
+                        } else if (contenidoBoton == "=") {
+                            //System.out.println("lsita op --" + operandos.toString());
+                            operandos.add(operando);
+
+                            double resultadoSuma = 0;
+                            //System.out.println(resultadoSuma);
+                            for (String op : operandos) {
+
+                                resultadoSuma += Double.parseDouble(op);
+                                //System.out.println(resultadoSuma);
+
+                            }
+
+                            String resultadoString = String.valueOf(resultadoSuma);
+
+                            areaTexto.setText(areaTexto.getText() + ((JButton) obj).getText() + resultadoString);
+                        } else {
+                            //System.out.println("operando antes -" + operando);
+                            operando += contenidoBoton;
+                            //System.out.println("operando despues - " + operando);
+                            //System.out.println("lsita -- " + operandos.toString());
+                            areaTexto.setText(areaTexto.getText() + ((JButton) obj).getText());
+                        }
                     }
                 }
             });
